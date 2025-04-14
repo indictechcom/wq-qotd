ac50e1b7e2f9b8b206049919397d973454b25c86
Agamya Samuel <github@agamya.dev>
2025-04-15 05:01:54 +0530

    Update response models in quote routes to use Quote instead of QuoteSchema for consistency

--- app/api/routers/quotes.py
+++ app/api/routers/quotes.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List, Optional
 from app.database.models import SessionLocal
-from app.schemas.schemas import QuoteSchema
+from app.schemas.schemas import Quote
 from app.database.crud import add_quote_to_db, get_quote_by_date, get_all_quotes
 from app.core.utils import fetch_quote_of_the_day_from_api
 from datetime import datetime
@@ -21,7 +21,7 @@ def get_db():
     finally:
         db.close()
 
-@router.get("/quote_of_the_day", response_model=QuoteSchema)
+@router.get("/quote_of_the_day", response_model=Quote)
 async def get_quote_of_the_day_route(db: Session = Depends(get_db)):
     """
     `/api/quote_of_the_day` route is used to get the Quote of the Day.
@@ -38,13 +38,13 @@ async def get_quote_of_the_day_route(db: Session = Depends(get_db)):
         quote = add_quote_to_db(db, quote_data)
     return quote
 
-@router.get("/quotes", response_model=List[QuoteSchema])
+@router.get("/quotes", response_model=List[Quote])
 async def get_all_quotes_route(
     page: int = 1, limit: int = 10, author: Optional[str] = None, db: Session = Depends(get_db)
 ):
     return get_all_quotes(db, page, limit, author)
 
-@router.get("/quotes/{date}", response_model=QuoteSchema)
+@router.get("/quotes/{date}", response_model=Quote)
 async def get_quote_by_date_route(date: str, db: Session = Depends(get_db)):
     try:
         target_date = datetime.strptime(date, "%Y-%m-%d")
